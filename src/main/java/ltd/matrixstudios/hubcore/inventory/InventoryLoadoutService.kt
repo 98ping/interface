c5296e33fbbd4cb10eb7baa179fc1747f1fe1177
98ping <67809373+98ping@users.noreply.github.com>
2022-08-10 18:37:06 -0700

    fix custom item handling

--- src/main/java/ltd/matrixstudios/hubcore/inventory/InventoryLoadoutService.kt
+++ src/main/java/ltd/matrixstudios/hubcore/inventory/InventoryLoadoutService.kt
@@ -3,11 +3,14 @@ package ltd.matrixstudios.hubcore.inventory
 import ltd.matrixstudios.hubcore.InterfacePlugin
 import ltd.matrixstudios.hubcore.selector.SelectorItem
 import ltd.matrixstudios.hubcore.selector.SelectorItemService
+import ltd.matrixstudios.hubcore.selector.menu.SelectorMenu
 import ltd.matrixstudios.hubcore.services.Service
 import ltd.matrixstudios.hubcore.utils.Chat
 import ltd.matrixstudios.hubcore.utils.ItemBuilder
 import me.lucko.helper.Events
 import org.bukkit.Material
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
 
 object InventoryLoadoutService : Service
@@ -34,7 +37,7 @@ object InventoryLoadoutService : Service
             val data = config.getInt("items.$item.data").toShort()
             val slot = config.getInt("items.$item.slot")
             val useAction = config.getBoolean("items.$item.useAction")
-            val commands = config.getStringList("items.$item.commands")
+            val commands = config.getStringList("items.$item.command")
 
             items[slot] = InventoryItem(
                 ItemBuilder.of(Material.getMaterial(material))
@@ -62,5 +65,25 @@ object InventoryLoadoutService : Service
 
                 player.updateInventory()
         }
+
+        Events.subscribe(PlayerInteractEvent::class.java)
+            .filter {
+                it.action == Action.RIGHT_CLICK_AIR || it.action == Action.RIGHT_CLICK_BLOCK
+            }
+            .filter { playerInteractEvent ->
+                playerInteractEvent.item != null && playerInteractEvent.item.type != Material.AIR && items.values.firstOrNull { it.itemStack.isSimilar(playerInteractEvent.item) } != null
+            }
+            .handler { event ->
+                val item = items.values.firstOrNull { it.itemStack.isSimilar(event.item) }
+                if (item != null)
+                {
+                    if (item.usesAction)
+                    {
+                        item.commands.forEach {
+                            event.player.performCommand(it)
+                        }
+                    }
+                }
+            }
     }
 }
--- src/main/java/ltd/matrixstudios/hubcore/store/DataStoreManager.kt
+++ src/main/java/ltd/matrixstudios/hubcore/store/DataStoreManager.kt
@@ -7,6 +7,7 @@ import gg.scala.store.connection.mongo.impl.details.DataStoreMongoConnectionDeta
 import gg.scala.store.connection.redis.AbstractDataStoreRedisConnection
 import gg.scala.store.connection.redis.impl.NoAuthDataStoreRedisConnection
 import gg.scala.store.connection.redis.impl.details.DataStoreRedisConnectionDetails
+import ltd.matrixstudios.hubcore.InterfacePlugin
 
 object DataStoreManager : ScalaDataStoreShared() {
     override fun debug(from: String, message: String) {
@@ -23,7 +24,7 @@ object DataStoreManager : ScalaDataStoreShared() {
     override fun getNewMongoConnection(): AbstractDataStoreMongoConnection
     {
         return UriDataStoreMongoConnection(
-            DataStoreMongoConnectionDetails("mongodb://localhost:27017", "interface")
+            DataStoreMongoConnectionDetails(InterfacePlugin.instance.config.getString("mongo"), "interface")
         )
     }
 }
